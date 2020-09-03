e0fe1aef6e9d9841d1e9519e6e89b800afa5c475
LeeGyung-jin <0l055225757@gmail.com>
2020-09-03 17:58:29 +0900

    20200903-2-1 ADMIN 추가 commit

--- src/main/java/com/kh/honeypoint/admin/member/model/vo/MemberMgt.java
+++ src/main/java/com/kh/honeypoint/admin/member/model/vo/MemberMgt.java
@@ -6,9 +6,10 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
-
+/*
 @Getter
 @Setter
+*/
 @NoArgsConstructor
 @AllArgsConstructor
 public class MemberMgt {
