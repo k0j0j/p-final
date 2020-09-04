45c5d970a842eb32a830bf3dbde33618b9a650d8
k0j0j <ssda332@naver.com>
2020-09-04 09:59:16 +0900

    200904-1 main 수정

--- src/main/java/com/kh/honeypoint/restaurant/model/vo/Review.java
+++ src/main/java/com/kh/honeypoint/restaurant/model/vo/Review.java
@@ -1,5 +1,8 @@
 package com.kh.honeypoint.restaurant.model.vo;
 
+import lombok.Data;
+
+@Data
 public class Review {
 	private int revNo;
 	private String revCn;
@@ -7,72 +10,4 @@ public class Review {
 	private char revStatus;
 	private String rNo;
 	private int mNo;
-	
-	public Review() {}
-
-	public Review(int revNo, String revCn, int score, char revStatus, String rNo, int mNo) {
-		super();
-		this.revNo = revNo;
-		this.revCn = revCn;
-		this.score = score;
-		this.revStatus = revStatus;
-		this.rNo = rNo;
-		this.mNo = mNo;
-	}
-
-	public int getRevNo() {
-		return revNo;
-	}
-
-	public void setRevNo(int revNo) {
-		this.revNo = revNo;
-	}
-
-	public String getRevCn() {
-		return revCn;
-	}
-
-	public void setRevCn(String revCn) {
-		this.revCn = revCn;
-	}
-
-	public int getScore() {
-		return score;
-	}
-
-	public void setScore(int score) {
-		this.score = score;
-	}
-
-	public char getRevStatus() {
-		return revStatus;
-	}
-
-	public void setRevStatus(char revStatus) {
-		this.revStatus = revStatus;
-	}
-
-	public String getrNo() {
-		return rNo;
-	}
-
-	public void setrNo(String rNo) {
-		this.rNo = rNo;
-	}
-
-	public int getmNo() {
-		return mNo;
-	}
-
-	public void setmNo(int mNo) {
-		this.mNo = mNo;
-	}
-
-	@Override
-	public String toString() {
-		return "Review [revNo=" + revNo + ", revCn=" + revCn + ", score=" + score + ", revStatus=" + revStatus
-				+ ", rNo=" + rNo + ", mNo=" + mNo + "]";
-	}
-	
-	
 }
